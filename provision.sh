14d99b64ba387ee11c8ad91b9f2a90648a4841ff
HatimAbdullah <HatimAlqiamh@gmail.com>
2020-08-13 11:56:37 +0300

    savve changes inside a dit

--- provision.sh
+++ provision.sh
@@ -3,9 +3,9 @@ sudo apt update -y && sudo apt install -y curl vim git docker.io
 sudo usermod -aG docker ubuntu
 touch nobodyknows
 date > nobodyknows
-touch index.html
-echo 'not touched yet' > index.html
-sudo docker image build -t small-ws /home/ubuntu/.
+touch nginx/index.html
+echo 'not touched yet' > nginx/index.html
+sudo docker image build -t small-ws /home/ubuntu/nginx/.
 sudo docker create --name hola -p 80:80 -v /home/ununtu/nginx:/work small-ws
 sudo systemctl enable web-nginx
 
