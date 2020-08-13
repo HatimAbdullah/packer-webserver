c59d90c44bcb5cd0b7fe8ca357bc30bef0472966
HatimAbdullah <HatimAlqiamh@gmail.com>
2020-08-13 10:23:27 +0300

    add webserver

--- provision.sh
+++ provision.sh
@@ -3,5 +3,9 @@ sudo apt update -y && sudo apt install -y curl vim git docker.io
 sudo usermod -aG docker ubuntu
 touch nobodyknows
 date > nobodyknows
-touch whereyouare
-pwd > whereyouare
+touch index.html
+echo 'not touched yet' > index.html
+sudo docker image build -t small-ws /home/ubuntu/.
+sudo docker create --name hola -p 6969:80 small-ws
+sudo systemctl enable web-nginx
+
